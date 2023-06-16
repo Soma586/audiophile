cfcf1fc8d831ecc3ae172df7deb9ec4a3cd82b8c
Sabo <sabo@Sabos-MBP.home>
2023-06-16 15:20:30 -0400

    tablet functionality is currently in progess

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,7 @@
 import './styles.scss'
 //import '../../scss/typography.scss'
 import Cart from '../../assets/shared/desktop/icon-cart.svg'
+import {ReactComponent as HamburgerIcon} from '../../assets/shared/tablet/icon-hamburger.svg'
 
 
 
@@ -10,7 +11,10 @@ const Nav = () => {
 
 
     return(
-        <div className="container navContainer d-flex justify-content-between">
+        <div className="container navContainer ">
+
+            {/* desktop view*/}
+            <div className="d-none d-lg-block d-lg-flex justify-content-between ">
             <span className="type__H6">Audiophile</span>
             
             <div className="d-flex justify-content-between type__H6 navList">
@@ -20,6 +24,17 @@ const Nav = () => {
                 <p>EARPHONES</p>
             </div>
             <img className="cart" src={Cart}/>
+            </div>
+
+            {/* tablet view */}
+
+            <div className="d-md-block d-md-flex justify-content-between d-lg-none" >
+                <HamburgerIcon className="test" onClick={() => console.log("hello")}/>
+                <span className="type__H6">Audiophile</span>
+
+                <img className="cart" src={Cart}/>
+            </div>
+
         </div>
     )
 }
